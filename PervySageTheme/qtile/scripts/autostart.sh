c421b785e613f5ebf3f8d0e1d57ecd1c42fabd5b
shawakaze <shawakaze@gmail.com>
2022-03-23 22:08:38 +0200

    autostart.sh changes in themes

--- PervySageTheme/qtile/scripts/autostart.sh
+++ PervySageTheme/qtile/scripts/autostart.sh
@@ -17,8 +17,8 @@ run pamac-tray &
 numlockx on &
 #blueman-applet &
 #run flameshot &
-#picom --config $HOME/.config/picom/picom.conf &
-picom --config .config/picom/picom-blur.conf --experimental-backends &
+picom --config $HOME/.config/picom/picom.conf &
+#picom --config .config/picom/picom-blur.conf --experimental-backends &
 #/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 dunst &
 #feh --randomize --bg-fill /usr/share/wallpapers/garuda-wallpapers/*
@@ -29,12 +29,12 @@ run volumeicon &
 nitrogen --restore &
 #run caffeine -a &
 #run vivaldi-stable &
-(sleep 5 &&  run firefox) &
-(sleep 10 && run thunderbird) &
+#(sleep 5 &&  run firefox) &
+#(sleep 10 && run thunderbird) &
 #run thunar &
 (sleep 15 && run dropbox) &
 (sleep 2 && run mpd) &
-(sleep 30 && run vlc)&
+#(sleep 30 && run vlc)&
 #run insync start &
 #run spotify &
 #run atom &
